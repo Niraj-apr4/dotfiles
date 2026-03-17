15304c3e31f9fcde5070709a4ced778834e433a2
niraj <niraj.apr4@gmail.com>
2026-03-18 02:05:19 +0530

    julia lsp

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -606,6 +606,9 @@ require('lazy').setup({
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
+        julials = {
+          root_markers = { 'Project.toml', 'JuliaProject.toml', '.git', '.' },
+        },
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
